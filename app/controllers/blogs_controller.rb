8c3d6cb4c029275397e48e6898b88bb14105554f
yokomaru <cabochetoganlan@gmail.com>
2025-07-23 18:22:48 +0900

    他のユーザの秘密のブログにアクセスできないように秘密かつ所有者ではなければRecordNotFoundエラーを起こす

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,7 +9,11 @@ class BlogsController < ApplicationController
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    return unless @blog.secret? && !@blog.owned_by?(current_user)
+
+    raise ActiveRecord::RecordNotFound
+  end
 
   def new
     @blog = Blog.new

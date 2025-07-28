5394879cee8dd764e94feaecbbb14ed3562a520f
yokomaru <cabochetoganlan@gmail.com>
2025-07-28 23:26:44 +0900

    html_safeではなくsanitizeを使用して安全なタグ(`<br>`)のみを出力するように修正

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    html_escape(blog.content).gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
+    sanitize(html_escape(blog.content).gsub("\n", '<br>'))
   end
 end
